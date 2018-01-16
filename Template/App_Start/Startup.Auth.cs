afcef8e88e098ceea6b9d27e3f6836e6071316d8
francois <francois@frontfundr.com>
2018-01-15 16:46:23 -0800

    facebook keys

--- Template/App_Start/Startup.Auth.cs
+++ Template/App_Start/Startup.Auth.cs
@@ -9,6 +9,7 @@ using Identity.Models;
 using System.Web.Script.Serialization;
 using System.Net.Http;
 using Microsoft.AspNet.Identity.Owin;
+using System.Configuration;
 
 namespace Template
 {
@@ -49,7 +50,11 @@ namespace Template
             // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
             // This is similar to the RememberMe option when you log in.
             app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
-        }
+
+           string FacebookAppId = ConfigurationManager.AppSettings["FacebookAppId"];
+           string FacebookAppSecret = ConfigurationManager.AppSettings["FacebookAppSecret"];
+
+    }
 
     }
 
