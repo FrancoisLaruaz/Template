f59cef961c8432a5164464e91f152936fe86bd81
francois <francois@frontfundr.com>
2018-01-19 09:55:36 -0800

    facebook login

--- Template/App_Start/Startup.Auth.cs
+++ Template/App_Start/Startup.Auth.cs
@@ -46,13 +46,6 @@ namespace Template
                 });
                 app.UseExternalSignInCookie(DefaultAuthenticationTypes.ExternalCookie);
 
-                // Enables the application to temporarily store user information when they are verifying the second factor in the two-factor authentication process.
-                app.UseTwoFactorSignInCookie(DefaultAuthenticationTypes.TwoFactorCookie, TimeSpan.FromMinutes(5));
-
-                // Enables the application to remember the second login verification factor such as phone or email.
-                // Once you check this option, your second step of verification during the login process will be remembered on the device where you logged in from.
-                // This is similar to the RememberMe option when you log in.
-                app.UseTwoFactorRememberBrowserCookie(DefaultAuthenticationTypes.TwoFactorRememberBrowserCookie);
 
                 #region Facebook Config
                 string FacebookAppId = ConfigurationManager.AppSettings["FacebookAppId"];
@@ -70,8 +63,8 @@ namespace Template
 
                     // Set requested scope
                     facebookOptions.Scope.Add("email");
-                    facebookOptions.Scope.Add("user_location");
-                    facebookOptions.Scope.Add("user_birthday");
+                    //facebookOptions.Scope.Add("user_location");
+                   // facebookOptions.Scope.Add("user_birthday");
 
                     app.UseFacebookAuthentication(facebookOptions);
 
